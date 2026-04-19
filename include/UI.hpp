d47867bd7d1d07c276dcbe19ab27bc84cd096d7b
Gautam Jangid <gautamjangid1728@gmail.com>
2026-04-19 21:57:30 +0530

    bugfix: copy-code-block on right click on selected text + interactive mode, 11th commit

--- include/UI.hpp
+++ include/UI.hpp
@@ -17,6 +17,7 @@ void updates_cb(Fl_Widget*, void*);
 
 void copy_block_by_index(int idx);
 void copy_cb(Fl_Widget*, void*);
+void copy_text_cb(Fl_Widget*, void*);
 void prev_code_cb(Fl_Widget*, void*);
 void next_code_cb(Fl_Widget*, void*);
 void timer_redraw_cb(void*);
